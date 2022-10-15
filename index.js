9c1e999f1f7f47cd79d59c77279eaf5c53309651
Phillip Wong <deepelegant99@gmail.com>
2022-10-15 15:33:17 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, './public/home.html'))
+})
+
+app.listen(3001);
